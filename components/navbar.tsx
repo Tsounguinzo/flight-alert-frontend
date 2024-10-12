835b96dcba70d480e131990b6f0a28a746c0e5ee
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2024-10-12 19:45:29 +0300

    updates

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -13,91 +14,74 @@ import {
 } from "@nextui-org/react";
 import NextLink from "next/link";
 
+import { siteConfig } from "@/utils/constants";
 import { HeaderLogo, SearchIcon } from "./icons";
 
-import { siteConfig } from "@/utils/constants";
-
-export const Navbar = async () => {
-
+export const Navbar = () => {
   return (
-      <NextUINavbar
-          maxWidth="xl"
-          position="sticky"
-          style={{backgroundColor: "#214CE7", color: "#fff"}}
+    <NextUINavbar
+      maxWidth="xl"
+      position="sticky"
+      style={{ backgroundColor: "#214CE7", color: "#fff" }}
+    >
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit items-center">
+          <HeaderLogo />
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <p className="font-extrabold text-2xl">FlyFast</p>
+          </NextLink>
+        </NavbarBrand>
+      </NavbarContent>
+      <div className="flex-1"></div>
+      <NavbarContent
+        as="div"
+        className="hidden sm:flex basis-1/5 sm:basis-full items-center justify-end"
       >
-        <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-          <NavbarBrand as="li" className="gap-3 max-w-fit items-center">
-            <HeaderLogo/>
-            <NextLink className="flex justify-start items-center gap-1" href="/">
-              <p className="font-extrabold text-2xl">FlyFast</p>
-            </NextLink>
-          </NavbarBrand>
-        </NavbarContent>
-        <NavbarContent
-            as="div"
-            className="hidden sm:flex basis-1/5 sm:basis-full items-center"
-            justify="end"
-        >
-          <ul className="hidden lg:flex gap-4 justify-start ml-2 w-full">
-            {siteConfig.navItems.map((item) => (
-                <NavbarItem key={item.href}>
-                  <Link href={item.href}>
-                    <Button
-                        as={Link}
-                        radius="full"
-                        style={{
-                          backgroundColor: "rgba(255, 255, 255, 0.1)",
-                          color: "#fff",
-                        }}
-                        type="button"
-                    >
-                      {item.label}
-                    </Button>
-                  </Link>
-                </NavbarItem>
-            ))}
-          </ul>
-          <Input
-              classNames={{
-                base: "max-w-full sm:max-w-[10rem] h-10",
-                mainWrapper: "h-full",
-                input: "text-small",
-                inputWrapper:
-                    "h-full font-normal text-default-500 bg-white dark:bg-default-500/20",
-              }}
-              endContent={<SearchIcon size={18}/>}
-              placeholder="Free Search"
-              radius="full"
-              size="sm"
-              type="search"
-          />
-        </NavbarContent>
+        <ul className="hidden lg:flex gap-4 justify-end ml-2 w-full">
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}>
+              <Link href={item.href}>
+                <Button
+                  radius="full"
+                  type="button"
+                  style={{
+                    backgroundColor: "rgba(255, 255, 255, 0.1)",
+                    color: "#fff",
+                  }}
+                >
+                  {item.label}
+                </Button>
+              </Link>
+            </NavbarItem>
+          ))}
+        </ul>
+      </NavbarContent>
 
-        <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-          <NavbarMenuToggle/>
-        </NavbarContent>
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        <NavbarMenuToggle />
+      </NavbarContent>
 
-        <NavbarMenu>
-          <div className="mx-4 mt-2 flex flex-col gap-2">
-            {siteConfig.navMenuItems.map((item, index) => (
-                <NavbarMenuItem key={`${item}-${index}`}>
-                  <Link
-                      color={
-                        index === 2
-                            ? "primary"
-                            : index === siteConfig.navMenuItems.length - 1
-                                ? "danger"
-                                : "foreground"
-                      }
-                      href="#"
-                      size="lg"
-                  >
-                    {item.label}
-                  </Link>
-                </NavbarMenuItem>
-            ))}
-          </div>
-        </NavbarMenu>
-      </NextUINavbar>
+      <NavbarMenu>
+        <div className="mx-4 mt-2 flex flex-col gap-2">
+          {siteConfig.navMenuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                color={
+                  index === 2
+                    ? "primary"
+                    : index === siteConfig.navMenuItems.length - 1
+                      ? "danger"
+                      : "foreground"
+                }
+                href="#"
+                size="lg"
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
+    </NextUINavbar>
   );
 };
