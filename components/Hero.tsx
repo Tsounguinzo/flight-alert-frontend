d5c3ee6a5343c005650c847437c4e84599d22ad2
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2024-10-23 10:40:12 -0400

    start with form

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,9 +2,36 @@
 
 import { AnimatePresence, LazyMotion, domAnimation, m } from "framer-motion";
 
-import { HeroBg } from "./icons";
+import { HeroBg, SwapDepArrIcon } from "./icons";
+import { Button, Select, SelectItem } from "@nextui-org/react";
+import PassengerSelector from "./livesearch/PassengerSelector";
+import Origin from "./livesearch/Origin";
+import Destination from "./livesearch/Destination";
+import { useState } from "react";
 
 export default function Hero() {
+  const flightTypes = [
+    { label: "Round Trip", key: "Round Trip" },
+    { label: "One Way", key: "One Way" },
+    { label: "Miles", key: "Miles" },
+  ];
+  const flightClass = [
+    { label: "Economy", key: "Economy" },
+    { label: "Premium", key: "Premium" },
+    { label: "Business", key: "Business" },
+    { label: "First", key: "First" },
+  ];
+
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
+
+  const handleSwap = () => {
+    const x = origin;
+    const y = destination;
+
+    setOrigin(y);
+    setDestination(x);
+  };
   return (
     <section
       className="relative h-[70vh] z-20 flex items-start gap-[18px] sm:gap-6 w-full"
@@ -56,8 +83,51 @@ export default function Hero() {
                   type: "spring",
                 }}
               >
-                <div className="p-28 w-[60%] transition-all duration-500 bg-blue-500 text-4xl rounded-md">
-                  Future search form
+                <div className=" flex flex-col w-[60%] transition-all duration-500 rounded-3xl border-1 border-[#457EFF]">
+                  <div className="p-5 flex items-center rounded-tl-3xl rounded-tr-3xl gap-3 bg-white w-full">
+                    <Select
+                      aria-label="type"
+                      className="w-[140px]"
+                      defaultSelectedKeys={["Round Trip"]}
+                      radius={"sm"}
+                    >
+                      {flightTypes.map((type) => (
+                        <SelectItem key={type.key}>{type.label}</SelectItem>
+                      ))}
+                    </Select>
+                    <PassengerSelector />
+                    <Select
+                      aria-label="class"
+                      className="w-[140px]"
+                      defaultSelectedKeys={["Economy"]}
+                      radius={"sm"}
+                    >
+                      {flightClass.map((fClass) => (
+                        <SelectItem key={fClass.key}>{fClass.label}</SelectItem>
+                      ))}
+                    </Select>
+                  </div>
+                  <div className="w-full h-[1px] bg-[#000000] opacity-[17%]"></div>
+
+                  <div className="p-5 flex items-center rounded-bl-3xl rounded-br-3xl gap-3 bg-[#F3F3F3] w-full">
+                    <div className="flex items-center gap-3 w-full">
+                      {/* Origin Autocomplete */}
+                      <Origin origin={origin} setOrigin={setOrigin} />
+                      {/* Swap Button */}
+                      <Button
+                        isIconOnly
+                        style={{ backgroundColor: "#F4F4F5" }}
+                        onClick={handleSwap}
+                      >
+                        <SwapDepArrIcon />
+                      </Button>
+                      {/* Destination Autocomplete */}
+                      <Destination
+                        destination={destination}
+                        setDestination={setDestination}
+                      />
+                    </div>
+                  </div>
                 </div>
               </m.div>
             </AnimatePresence>
--- components/livesearch/Origin.tsx
+++ components/livesearch/Origin.tsx
@@ -1,6 +1,6 @@
 import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
 import { useAsyncList } from "@react-stately/data";
-import { FaPlaneDeparture } from "react-icons/fa";
+import { DepartureIcon } from "../icons";
 
 // Define the type for the airport item
 export interface AirportItem {
@@ -31,60 +31,70 @@ export default function Origin({ origin, setOrigin }: OriginProps) {
   });
 
   return (
-    <Autocomplete
-      aria-label="origin"
-      inputValue={origin || list.filterText}
-      isLoading={list.isLoading}
-      items={list.items}
-      label="From"
-      placeholder="Select origin"
-      startContent={<FaPlaneDeparture style={{ marginRight: 4 }} />}
-      onInputChange={(text) => {
-        list.setFilterText(text);
-        setOrigin(""); // Clear the selected value when typing
-      }}
-      onSelectionChange={(key: any) => {
-        // Find the selected item and update input value
-        const selectedItem = list.items.find(
-          (item) => `${item.city_name} (${item.airport_code})` === key,
-        );
+    <div className="flex flex-col gap-2">
+      <h3 className="text-[#868C98] text-small">From</h3>
+      <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
+        <Autocomplete
+          aria-label="origin"
+          inputValue={origin || list.filterText}
+          isLoading={list.isLoading}
+          items={list.items}
+          placeholder="Origin"
+          startContent={<DepartureIcon style={{ marginRight: 4 }} />}
+          onInputChange={(text) => {
+            list.setFilterText(text);
+            setOrigin(""); // Clear the selected value when typing
+          }}
+          onSelectionChange={(key: any) => {
+            // Find the selected item and update input value
+            const selectedItem = list.items.find(
+              (item) => `${item.city_name} (${item.airport_code})` === key
+            );
 
-        if (selectedItem) {
-          setOrigin(`${selectedItem.city_name} (${selectedItem.airport_code})`);
-        }
-      }}
-    >
-      {(item) => (
-        <AutocompleteItem
-          key={`${item.city_name} (${item.airport_code})`}
-          textValue={`${item.city_name} (${item.airport_code})`}
+            if (selectedItem) {
+              setOrigin(
+                `${selectedItem.city_name} (${selectedItem.airport_code})`
+              );
+            }
+          }}
+          color="default"
+          classNames={{
+            base: "w-[200px] h-[40px]",
+          }}
         >
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <div className="flex flex-col flex-2">
-              <p>{item.airport_name}</p>
-              <span className="text-gray-500">
-                {item.city_name}, {item.country_name}
-              </span>
-            </div>
-            <div style={{ alignSelf: "flex-start" }}>
-              <span
-                className="text-white text-xs px-2 py-2 rounded-full"
+          {(item) => (
+            <AutocompleteItem
+              key={`${item.city_name} (${item.airport_code})`}
+              textValue={`${item.city_name} (${item.airport_code})`}
+            >
+              <div
                 style={{
-                  backgroundColor: "#214CE7",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
                 }}
               >
-                {item.airport_code}
-              </span>
-            </div>
-          </div>
-        </AutocompleteItem>
-      )}
-    </Autocomplete>
+                <div className="flex flex-col flex-2">
+                  <p>{item.airport_name}</p>
+                  <span className="text-gray-500">
+                    {item.city_name}, {item.country_name}
+                  </span>
+                </div>
+                <div style={{ alignSelf: "flex-start" }}>
+                  <span
+                    className="text-white text-xs px-2 py-2 rounded-full"
+                    style={{
+                      backgroundColor: "#214CE7",
+                    }}
+                  >
+                    {item.airport_code}
+                  </span>
+                </div>
+              </div>
+            </AutocompleteItem>
+          )}
+        </Autocomplete>
+      </div>
+    </div>
   );
 }
